8dce27c3de69d89bf48ad2f0f90411e4a1601250
SANLAB\ismail.turkmenoglu <ismail.turkmenoglu@sanlab.net>
2022-03-03 17:03:31 +0300

    Micro ORM in C#

---
+++ src/GooORM/Goo.UnitTest/Tests/CrudTest.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Goo.UnitTest.Entities;
+
+namespace Goo.UnitTest.Tests
+{
+    /// <summary>
+    /// GooContext sınıfı üzerinden insert/update ve delete işlemlerinizi yapabilirsiniz.
+    /// Using the GooContext class you can perform basic operations like insert/update or delete an entity.
+    /// </summary>
+    [TestClass]
+    public class CrudTest
+    {
+        private GooContext gooContext;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            gooContext = new GooContext();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            gooContext = null;
+        }
+
+        #region Create/Update/Delete Test Methods
+        /// <summary>
+        /// Bu metot nesne eklemenizi sağlar.
+        /// This method inserts the entity.
+        /// </summary>
+        [TestMethod]
+        public void InsertAnEntity()
+        {
+            Categories category = new Categories()
+            {
+                CategoryName = "Computer",
+                Description = "Insert test"
+            };
+
+            gooContext.Categories.Insert(category);
+
+            int result = gooContext.SubmitChanges();
+
+            Assert.AreNotEqual(result, -1);
+        }
+
+        /// <summary>
+        /// Bu metot ile bir nesneyi transaction işlemi ile eklemenizi sağlar.
+        /// This method inserts the entity with transaction.
+        /// </summary>
+        [TestMethod]
+        public void InsertAnEntityWithTransaction()
+        {
+            // Connection'a erişmeniz gereken durumlarda bağlantıyı sizin açmanız gerekmektedir.
+            // In case you need to reference the connection object, you need to open and manage the connection.
+            gooContext.OrmConfiguration.Connection.Open();
+
+            using (var transaction = gooContext.OrmConfiguration.Connection.BeginTransaction())
+            {
+                // Transaction nesnesini orm'e kullanacağımızı belirtmeliyiz.
+                // You must pass in the transaction object's reference.
+                gooContext.OrmConfiguration.UseTransaction(transaction);
+
+                Categories category = new Categories()
+                {
+                    CategoryName = "Computer",
+                    Description = "Insert test with transaction"
+                };
+
+                gooContext.Categories.Insert(category);
+
+                int result = gooContext.SubmitChanges();
+
+                if (result > -1)
+                    transaction.Commit();
+                else
+                    transaction.Rollback();
+            }
+        }
+
+        /// <summary>
+        /// Bu metot bir nesneyi güncellemenizi sağlar.
+        /// This method updates an entity.
+        /// </summary>
+        [TestMethod]
+        public void UpdateAnEntity()
+        {
+            Categories category = gooContext.Categories.FirstOrDefault();
+
+            category.CategoryName = "Computer Update";
+            category.Description = "Update test";
+
+            gooContext.Categories.Update(category);
+
+            int result = gooContext.SubmitChanges();
+
+            Assert.AreNotEqual(result, -1);
+        }
+
+        /// <summary>
+        /// Bu metot bir nesneyi transaction işlemi ile güncellemenizi sağlar.
+        /// This method updates an entity with transaction.
+        /// </summary>
+        [TestMethod]
+        public void UpdateAnEntityWithTransaction()
+        {
+            // Connection'a erişmeniz gereken durumlarda bağlantıyı sizin açmanız gerekmektedir.           
+            // In case you need to reference the connection object, you need to open and manage the connection.
+            gooContext.OrmConfiguration.Connection.Open();
+
+            using (var transaction = gooContext.OrmConfiguration.Connection.BeginTransaction())
+            {
+                // Transaction nesnesini orm'e kullanacağımızı belirtmeliyiz.
+                // You must pass in the transaction object's reference.
+                gooContext.OrmConfiguration.UseTransaction(transaction);
+
+                Categories category = gooContext.Categories.FirstOrDefault();
+
+                category.CategoryName = "Computer Update";
+                category.Description = "Update test";
+
+                gooContext.Categories.Update(category);
+
+                int result = gooContext.SubmitChanges();
+
+                if (result > -1)
+                    transaction.Commit();
+                else
+                    transaction.Rollback();
+            }
+        }
+
+        /// <summary>
+        /// Bu metot bir nesne silmenizi sağlar.
+        /// This method deletes an entity.
+        /// </summary>
+        [TestMethod]
+        public void DeleteAnEntity()
+        {
+            Categories category = gooContext.Categories.FirstOrDefault();
+
+            gooContext.Categories.Delete(category);
+
+            int result = gooContext.SubmitChanges();
+
+            Assert.AreNotEqual(result, -1);
+        }
+
+        /// <summary>
+        /// Bu metot Insert sorgusunu CommandType ' ı text olarak çalıştırır.
+        /// </summary>
+        [TestMethod]
+        public void CustomExecuteNonQueryForText()
+        {
+            var q = "Insert Into Categories(CategoryName) Values ('Example')";
+            var res = this.gooContext.ExecuteNonCustomQuery(query: q, commandType: System.Data.CommandType.Text);
+
+           
+            var expected = res > 0;
+
+            Assert.IsTrue(expected);
+        }
+
+        /// <summary>
+        /// Bu metot Insert sorgusunu CommandType ' ı StoreProcedure olarak parametreli bir şekilde çalıştırır.
+        /// Not Bu testi geçebilmeniz için aşağıdaki SP ' i local server'da execute etmeniz gerekmektedir.
+        /// </summary>
+        /// 
+
+        //Create Procedure Insert_Categories(
+        //@catName NVARCHAR(15),
+        //@desc NTEXT
+        //)
+        //As
+        //Begin
+        //Insert Into Categories (CategoryName,[Description]) Values (
+        //@catName,
+        //@desc
+        //)
+        //End
+
+        [TestMethod]
+        public void CustomExecuteNonQueryForSp()
+        {
+            var spName = "Insert_Categories";
+            var sqlParams = new System.Data.SqlClient.SqlParameter[2];
+            {
+                sqlParams[0] = new System.Data.SqlClient.SqlParameter("@catName", "Example");
+                sqlParams[1] = new System.Data.SqlClient.SqlParameter("@desc", "Example-Description");
+            }
+
+            var res = this.gooContext.ExecuteNonCustomQuery(query: spName, commandType: System.Data.CommandType.StoredProcedure, sqlParams: sqlParams);
+
+            var expected = res > 0;
+
+            Assert.IsTrue(expected);
+        }
+
+        #endregion
+    }
+}
---
+++ src/GooORM/Goo/OrmCore/DBProviders/Interfaces/DBContextBase.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Goo.OrmCore
+{
+    public abstract class DBContextBase
+    {
+        #region Constructor
+        protected DBContextBase()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        private OrmConfiguration _ormConfiguration = new OrmConfiguration();
+        public OrmConfiguration OrmConfiguration
+        {
+            get
+            {
+                return _ormConfiguration;
+            }
+            set { _ormConfiguration = value; }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Bu metot kendi kompleks sorgunuzu oluşturabilmenizi sağlar.
+        /// This method can be used to perform complex queries.
+        /// </summary>
+        /// <param name="query">Query statement.</param>
+        /// <param name="parameters">Query parameters. This is not a required.</param>
+        /// <returns>This method will return DbDataReader object.</returns>
+        public DbDataReader ExecuteCustomQuery(string query, object parameters = null)
+        {
+            var dbManager = new DBManager(OrmConfiguration);
+
+            dbManager.CreateCommand(query);
+
+            if (parameters != null)
+                dbManager.AddParameter(parameters);
+
+            var reader = dbManager.ExecuteReader();
+
+            return reader;
+        }
+
+        /// <summary>
+        /// Bu metot kendi insert update delete sorgularınızı argüman olarak aldığı commandType nesnesi sayesinde ister StoreProcedure ister text isterseniz de table direct olarak oluşturmanızı sağlar.
+        /// Not geçirilmesi beklenen SqlParameter[] argümanı params key ile işaretlendiği için kullanıma zorunlu kılınmamıştır.
+        /// </summary>
+        /// <param name="query">Query statement.</param>
+        /// <param name="commandType">Query Command Type.</param>
+        /// <param name="sqlParams">SqlParameters.</param>
+        /// <returns>Number of Rows Affected.</returns>
+        public int ExecuteNonCustomQuery(string query, System.Data.CommandType commandType, params System.Data.SqlClient.SqlParameter[] sqlParams)
+        {
+            var dbManager = new DBManager(OrmConfiguration);
+
+            dbManager.CreateCommand(query, commandType);
+
+            dbManager.AddParameter(sqlParams);
+
+            var result = dbManager.ExecuteNonQuery();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Ekleme, silme veya güncelleme gibi yapılmış olan işlemleri, veritabanında uygulamayı sağlar.
+        /// Allows you to perform insert, delete or update operations on the database.
+        /// </summary>
+        /// <returns></returns>
+        public int SubmitChanges()
+        {
+            var dbManager = new DBManager(OrmConfiguration);
+
+            dbManager.CreateCommand(DBManager.CommandText);
+
+            if (DBManager.CommandParameters != null && DBManager.CommandParameters.Count > 0)
+                foreach (var loopParameter in DBManager.CommandParameters)
+                {
+                    dbManager.AddParameter(loopParameter.Key.ToString(), loopParameter.Value);
+                }
+
+            int status = dbManager.ExecuteNonQuery();
+
+            return status;
+        }
+        #endregion
+    }
+}
